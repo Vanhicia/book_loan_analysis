0395bee90cbd3cdf00615a4ddb6e1ca2bc9dda7d
Vanhicia <vanhulle@etud.insa-toulouse.fr>
2019-11-30 21:09:00 +0100

    add plot top des auteurs

--- graph_script.R
+++ graph_script.R
@@ -8,7 +8,10 @@ df_all_Paris <- read.csv("dataset_Paris.csv", header=TRUE, sep=";")
 #Toulouse
 
 # Nettoyage :
-df_book_Tls <- rename(df_book_Tls, c(ANNEE="Année", TITRE="Titre"))
+df_book_Tls <- rename(df_book_Tls, c(ANNEE="Année", TITRE="Titre", AUTEUR="Auteur"))
+
+# nettoyer noms des auteurs : suppr infos sur date de naissance entre parenthèses
+df_book_Tls$Auteur <- gsub("[(].*[)]","",df_book_Tls$Auteur)
 
 # rassembler les catégories similaires
 
@@ -32,3 +35,12 @@ df_book_Tls <- na.omit(df_book_Tls)
 df_total_par_cat_Tls <- ddply(df_book_Tls, .(Année,Cat.1,Cat.2), summarize, Total.nbre.prêts=sum(Nbre.de.prêts))
 #TODO : enlever lignes sans catégorie 1
 ggplot(df_total_par_cat_Tls, aes(x=Année, y=Total.nbre.prêts, color=Cat.2)) + geom_line() + facet_grid(.~Cat.1)
+
+# top 10 des auteurs
+auteurs <- ddply(df_book_Tls, .(Auteur,Année), summarize, Nb.prêts=sum(Nbre.de.prêts))
+auteurs <- auteurs[auteurs$Auteur != "-",]
+auteurs2018 <- auteurs[auteurs$Année == 2018,]
+top10auteurs2018 <- head(auteurs2018[order(auteurs2018$Nb.prêts, decreasing = TRUE),],10)
+grepl("Sobral, Patrick", top10auteurs2018$Auteur)
+ggplot(top10auteurs2018, aes(x=Auteur, y=Nb.prêts)) + geom_bar(stat="identity", show.legend = FALSE) + coord_flip() 
+# TODO : parvenir à rassembler les lignes avec le m$eme auteur
