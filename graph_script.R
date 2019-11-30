95303f296d517a30ce0dfd5ab1578149555460bf
Vanhicia <vanhulle@etud.insa-toulouse.fr>
2019-11-30 18:32:21 +0100

    correct

--- graph_script.R
+++ graph_script.R
@@ -1,14 +1,14 @@
 library(ggplot2)
-#library(plyr)
-library(dplyr)
+library(plyr)
+library(reshape2)
 
-df_book_Tls = read.csv("dataset_Toulouse.csv", header=TRUE, sep=";")
-df_all_Paris = read.csv("dataset_Paris.csv", header=TRUE, sep=";")
+df_book_Tls <- read.csv("dataset_Toulouse.csv", header=TRUE, sep=";")
+df_all_Paris <- read.csv("dataset_Paris.csv", header=TRUE, sep=";")
 
 #Toulouse
 
 # Nettoyage :
-df_book_Tls = rename(df_book_Tls, Année=ANNEE, Titre=TITRE)
+df_book_Tls <- rename(df_book_Tls, c(ANNEE="Année", TITRE="Titre"))
 
 # rassembler les catégories similaires
 
@@ -26,8 +26,9 @@ df_book_Tls$Cat.2[df_book_Tls$Cat.2 %in% c("ROM", "TE")] <- "ROM"
 #df_book_Tls$Cat.2[df_book_Tls$Cat.2 %in% c("SF")] <- "SCIENCE FICTION"
 
 # supprimer les lignes où le nombre de prêts n'est pas indiqué
-df_book_Tls = na.omit(df_book_Tls) 
+df_book_Tls <- na.omit(df_book_Tls) 
 
 # plot l'évolution du nombre de prêts par catégorie au cours des années
-df_total_par_cat_Tls = ddply(df_book_Tls, .(Année,Cat.1,Cat.2), summarize, Total.nbre.prêts=sum(Nbre.de.prêts))
+df_total_par_cat_Tls <- ddply(df_book_Tls, .(Année,Cat.1,Cat.2), summarize, Total.nbre.prêts=sum(Nbre.de.prêts))
+#TODO : enlever lignes sans catégorie 1
 ggplot(df_total_par_cat_Tls, aes(x=Année, y=Total.nbre.prêts, color=Cat.2)) + geom_line() + facet_grid(.~Cat.1)
